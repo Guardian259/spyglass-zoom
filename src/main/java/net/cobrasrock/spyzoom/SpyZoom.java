3aac86ce1dfbe39c99af5ddab5b812dcc50f2e7f
Guardian <PrimaryAppLogin@gmail.com>
2025-03-05 17:50:28 -0600

    Minor Import Cleanup

--- src/main/java/net/cobrasrock/spyzoom/SpyZoom.java
+++ src/main/java/net/cobrasrock/spyzoom/SpyZoom.java
@@ -2,9 +2,6 @@ package net.cobrasrock.spyzoom;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.item.Items;
-import org.spongepowered.asm.mixin.Unique;
 
 public class SpyZoom implements ModInitializer {
 	public static float zoom = 0.1f;
